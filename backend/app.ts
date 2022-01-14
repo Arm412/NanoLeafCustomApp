da67a8b69395d8a2a3f01ae0320de12e36c3db2d
Adam Mitro <adam.mitro@gmail.com>
2022-01-14 17:28:51 -0500

    Fix Prettier Merge

--- backend/app.ts
+++ backend/app.ts
@@ -8,8 +8,12 @@ import ip from 'ip';
 import axios, { AxiosRequestConfig } from 'axios';
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-import {updateEffectsList, deleteEffectsList, getStoredEffects} from "./helpers/dbHelpers";
-import {fetchEffectsList} from "./helpers/leafHelpers";
+import {
+  updateEffectsList,
+  deleteEffectsList,
+  getStoredEffects
+} from './helpers/dbHelpers';
+import { fetchEffectsList } from './helpers/leafHelpers';
 
 dotenv.config();
 
@@ -25,18 +29,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.get('/getNanoLeafData', async (req: Request, res: Response) => {
-	res.send(await getStoredEffects());
+  res.send(await getStoredEffects());
 });
 
-app.post('/setCurrentEffect', (req, res) => {
+// Sends command to update the current effect
+app.post('/setCurrentEffect', (req: Request, res: Response) => {
   console.log(req.body.effect);
   let chosenEffect = req.body.effect;
   var data = '{"select" : "' + chosenEffect + '"}';
-// Sends command to update the current effect
-app.post('/setCurrentEffect', (req: Request, res: Response) => {
-	console.log(req.body.effect);
-	let chosenEffect = req.body.effect; 
-	var data = '{"select" : "' + chosenEffect + '"}';
 
   var config: AxiosRequestConfig = {
     method: 'put',
@@ -49,28 +49,27 @@ app.post('/setCurrentEffect', (req: Request, res: Response) => {
     data: data
   };
 
-	axios(config)
-	.then((response: any) => {
-		console.log("Success");
-  		console.log(JSON.stringify(response.data));
-	})
-	.catch((error: string) => {
-  		console.log(error);
-	});
+  axios(config)
+    .then((response: any) => {
+      console.log('Success');
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error: string) => {
+      console.log(error);
+    });
 });
 
 // Updates the effects stored in the db
 app.post('/updateEffectsList', async (req: Request, res: Response) => {
-
-	// Call function to get list of Nanoleaf effects
-	const retrievedEffectList:Array<String> = await fetchEffectsList() || [];
-	await deleteEffectsList();
-	await updateEffectsList(retrievedEffectList);
+  // Call function to get list of Nanoleaf effects
+  const retrievedEffectList: Array<String> = (await fetchEffectsList()) || [];
+  await deleteEffectsList();
+  await updateEffectsList(retrievedEffectList);
 });
 
 const PORT = process.env.PORT || 80;
 
 app.listen(PORT, async () => {
-	console.log(ip.address());
-	console.log('Listening on Port: ' + PORT);
+  console.log(ip.address());
+  console.log('Listening on Port: ' + PORT);
 });
